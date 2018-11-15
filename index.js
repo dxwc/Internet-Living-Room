45c34f22ed09d5b8d8f8f174768741ac2c0dbf97
Ying Dong <yingdoi7@gmail.com>
2018-11-14 21:50:09 -0500

    modify index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/**
 const app = require('express')();
 
 app.use(require('body-parser').urlencoded({ extended: false }));
@@ -55,4 +56,63 @@ function start()
 if(!process.env.TESTING) start();
 
 module.exports.app    = app;
-module.exports.start  = start;
+module.exports.start  = start;**/
+const bodyParser = require('body-parser');
+const express = require('express');
+const model = require('./model');
+
+const PORT = process.env.PORT || 9001;
+
+const app = express();
+const cookieParser = require('cookie-parser');
+const expressSession = require('express-session');
+const passport = require('./middleware/auth');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// Uncomment the following if you want to serve up static assets.
+// (You must create the public folder)
+/*
+app.use(express.static('./public'));
+*/
+
+// Uncomment the following if you want to use handlebars
+// on the backend. (You must create the views folder)
+/*
+const exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({
+  layoutsDir: './views/layouts',
+  defaultLayout: 'main',
+}));
+app.set('view engine', 'handlebars');
+app.set('views', `${__dirname}/views/`);
+*/
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
+app.use(expressSession(({
+  secret: 'change this part',
+  resave: false,
+  saveUninitialized: true,
+})));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Load up all of the controllers
+const controller = require('./controller');
+app.use(controller)
+
+
+// First, make sure the Database tables and models are in sync
+// then, start up the server and start listening.
+model.sequelize.sync({force: false})
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is up and running on port: ${PORT}`)
+    });
+  });
