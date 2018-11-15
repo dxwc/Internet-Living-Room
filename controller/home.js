37184810267513e916be4d04306074b624a956a9
Ying Dong <yingdoi7@gmail.com>
2018-11-15 15:10:43 -0500

    update controller home

--- controller/home.js
+++ controller/home.js
@@ -1,8 +1,34 @@
-let router = require('express').Router();
+const express = require('express');
+const model = require('../model');
 
-router.get('/', (req, res) =>
-{
-    res.send('Hello World!');
+const router = express.Router();
+
+
+router.get('/', (req, res) => {
+  res.json({
+    msg: "Successful GET to '/' route"
+  });
 });
+
+router.post('/', (req, res) => {
+  res.json({
+    msg: "Successful POST to '/' route"
+  });
+});
+
+router.put('/:id', (req, res) => {
+  res.json({
+    msg: "Successful PUT to '/' route",
+    id: req.params.id
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  res.json({
+    msg: "Successful DELETE to '/' route",
+    id: req.params.id
+  });
+});
+
 
 module.exports = router;
