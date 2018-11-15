8d15357d2589eb51a1419bd46c3db8850f1697af
Ying Dong <yingdoi7@gmail.com>
2018-11-14 21:48:36 -0500

    add channel model

---
+++ model/channel.js
@@ -0,0 +1,23 @@
+const User = require("./user.js");
+//const Sequelize = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+	const Channel = sequelize.define('channel', {
+		id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        host: {
+        	type: DataTypes.STRING,
+        	allowNull: false
+        }
+	});
+
+	Channel.associate = (model) => {
+		model.Channel.hasMany(model.User);
+	}
+	return Channel;
+}
+
+
+
