fcb7f177689dd16166d18f600187579302a8467f
Alan Lau <alanlau97@gmail.com>
2018-12-04 01:19:01 -0500

    added votes model

--- controller/api_0.0.0/video_submission.js
+++ controller/api_0.0.0/video_submission.js
@@ -3,5 +3,6 @@ let op          = require('../../model/api_operations');
 
 router.post('/api/0.0.0/submit_video', (req, res) => {
     // verify all the data come thru
+    // need to have the url, by, and channel
     // if valid: use the op to create a new enrty.
 });
--- model/setup.js
+++ model/setup.js
@@ -1,135 +1,168 @@
 const Sequelize = require('sequelize');
-const bcrypt    = require('bcrypt');
+const bcrypt = require('bcrypt');
 
 const sequelize = new Sequelize
-(
+    (
     'ilr',
     'ilr_admin',
     'ilr_pass',
     {
-        host : 'localhost',
-        dialect : 'postgres',
-        logging : false,
-        operatorsAliases : false // removes a deprecation warning
+        host: 'localhost',
+        dialect: 'postgres',
+        logging: false,
+        operatorsAliases: false // removes a deprecation warning
     }
-);
+    );
 
 const user = sequelize.define
-(
+    (
     'user',
     {
-        id :
+        id:
         {
-            type : Sequelize.UUID,
-            defaultValue : Sequelize.UUIDV4,
+            type: Sequelize.UUID,
+            defaultValue: Sequelize.UUIDV4,
             primaryKey: true
         },
-        uname :
+        uname:
         {
-            type : Sequelize.TEXT,
-            unique : true,
-            allowNull : false
+            type: Sequelize.TEXT,
+            unique: true,
+            allowNull: false
         },
-        upass :
+        upass:
         {
-            type : Sequelize.TEXT,
-            allowNull : false
+            type: Sequelize.TEXT,
+            allowNull: false
         },
-        fname :
+        fname:
         {
-            type : Sequelize.TEXT,
-            allowNull : true
+            type: Sequelize.TEXT,
+            allowNull: true
         },
-        lname :
+        lname:
         {
-            type : Sequelize.TEXT,
-            allowNull : true
+            type: Sequelize.TEXT,
+            allowNull: true
         }
     }
-);
+    );
 
 const channel = sequelize.define
-(
+    (
     'channel',
     {
-        id :
+        id:
         {
-            type : Sequelize.UUID,
-            defaultValue : Sequelize.UUIDV4,
+            type: Sequelize.UUID,
+            defaultValue: Sequelize.UUIDV4,
             primaryKey: true
         },
-        host :
+        host:
         {
-            type : Sequelize.UUID,
-            references :
+            type: Sequelize.UUID,
+            references:
             {
-                model : user,
-                key : 'id'
+                model: user,
+                key: 'id'
             }
         },
-        capacity :
+        capacity:
         {
-            type : Sequelize.INTEGER,
-            defaultValue : 1
+            type: Sequelize.INTEGER,
+            defaultValue: 1
         }
     }
-);
+    );
 
 const video = sequelize.define
-(
+    (
     'video',
     {
-        id :
+        id:
         {
-            type : Sequelize.UUID,
-            defaultValue : Sequelize.UUIDV4,
+            type: Sequelize.UUID,
+            defaultValue: Sequelize.UUIDV4,
             primaryKey: true
         },
-        url_of_video :
+        url_of_video:
         {
             type: Sequelize.TEXT
         },
-        person :  // the person who submit the video
+        person:  // the person who submit the video
         {
-            type : Sequelize.UUID,
-            references :
+            type: Sequelize.UUID,
+            references:
             {
-                model : user,
-                key : 'id'
+                model: user,
+                key: 'id'
             }
         },
-        channel :  // the video from which channel
+        channel:  // the video from which channel
         {
-            type : Sequelize.UUID,
-            references :
+            type: Sequelize.UUID,
+            references:
             {
-                model: channel,key: 'id'
+                model: channel, key: 'id'
             }
         }
     }
+    );
+
+const votes = sequelize.define('votes', {
+    // each person will vote for only one video in each channel
+    // same person cannot reappear in the same channel again with different vote
+    // same person in the same channel cannot have the same vote
+    person: {
+        // the person who vote
+        type: Sequelize.TEXT,
+        unique: 'compositeIndex', // this allow person + vote + channel become a unique index
+        allowNull: false,
+        references: {
+            model: user,
+            key: 'uname'
+        }
+    },
+    vote: {
+        // the video that the person chose
+        type: Sequelize.UUID,
+        allowNull: false,
+        unique: 'compositeIndex',
+        references: {
+            model: video,
+            key: 'id'
+        }
+    },
+    channel: {
+        // the channel which the person and video are in
+        type: Sequelize.UUID,
+        allowNull: false,
+        unique: 'compositeIndex',
+        references: {
+            model: channel,
+            key: 'id'
+        }
+    }
+}
 );
 
-function connect()
-{
-    return new Promise((resolve, reject) =>
-    {
+function connect() {
+    return new Promise((resolve, reject) => {
         sequelize.sync
-        ({
-            logging : false,
-            // force: true, // deletes all data
-            // alter : true // deleted data where necessary
-        })
-        .then(() =>
-        {
-            console.info('- DB server connection started');
-            return resolve(sequelize);
-        })
-        .catch((err) =>
-        {
-            console.error('Error setting up tables');
-            console.error(err);
-            process.exit(1);
-        });
+            ({
+                logging: false,
+                // force: true, // deletes all data
+                // alter : true // deleted data where necessary
+            })
+            .then(() => {
+                console.info('- DB server connection started');
+                return resolve(sequelize);
+            })
+            .catch((err) => {
+                console.error('Error setting up tables');
+                console.error(err);
+                process.exit(1);
+            });
     });
 }
 
