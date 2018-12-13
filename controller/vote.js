3a7a6f15166ced93bed2c9d3d7700115176a1d11
Ying Dong <yingdoi7@gmail.com>
2018-12-13 14:09:43 -0500

    update vote controller

--- controller/vote.js
+++ controller/vote.js
@@ -2,17 +2,15 @@ let router = require('express').Router();
 let op     = require('../model/api_operations');
 
 router.post
-('/vote', require('../middleware/logged_in_only.js'), (req, res) =>{
-	if( typeof(req.body.username) === 'string' &&
-		typeof(req.body.channel_id) === 'string' &&
+('/vote', require('../middleware/logged_in_only.js'), 
+	(req, res) =>{
+	if( typeof(req.body.channel_id) === 'string' &&
 		typeof(req.body.video_id) === 'string' && 
 		typeof(req.body.vote) === 'number')
 	{
 
-		op.validate_vote(req.body.username, req.body.channel_id, req.body.video_id, req.body.vote)
+		op.validate_vote(req.session.passport.user.id, req.body.channel_id, req.body.video_id, req.body.vote)
 		.then((result) =>{
-
-			console.log(result);
 			return res.status(201).json({msg: "vote", success: true});
 		})
 		.catch((err) => {
@@ -22,7 +20,7 @@ router.post
 					reason_code: -1,
 					reason_text: 'already voted for this video',
 				}
-				return res.status(201).json(out);
+				return res.status(400).json(out);
 			} else {
 				return res.status(500).json
 	                ({
@@ -43,15 +41,13 @@ router.post
 }
 );
 
-router.get('/vote/:username/:channel_id/:video_id', (req, res) => {
+router.get('/user_vote/:user_id/:channel_id/:video_id/', (req, res) => {
 
 	//console.log(typeof(parseInt(req.params.vote_id)));
-
-
-		op.get_user_vote(req.params.username, req.params.channel_id, req.params.video_id)
+	//req.session.passport.user.id
+		op.get_user_vote(req.params.user_id, req.params.channel_id, req.params.video_id)
 		.then((result) => {
-			result.success = true;
-			return res.status(201).json(result);
+			return res.status(200).json({success: true, vote: result});
 
 		}).catch((err) => {
 			throw err;
@@ -60,5 +56,25 @@ router.get('/vote/:username/:channel_id/:video_id', (req, res) => {
 
 
 	});
+
+router.get('/total_vote/:channel_id/:video_id', (req, res) =>{
+	op.get_total_vote(req.params.channel_id, req.params.video_id)
+	.then((result) => {
+		return res.status(200).json({success: true, vote: result});
+	}).catch((err) =>{
+		throw err;
+	})
+});
+
+router.get('/highest_vote/:channel_id', (req, res) =>{
+	op.highest_vote(req.params.channel_id)
+	.then((video) =>{
+		return res.status(200).json({success: true, video_id: video[0], vote: video[1]});
+	})
+	.catch((err) => {
+		console.log(typeof(err));
+		throw err;
+	})
+});
 
 module.exports = router;
